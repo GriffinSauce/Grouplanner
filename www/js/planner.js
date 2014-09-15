46c9503492f596fb2fce62462cf5a67c9df3bd59
GriffinSauce <joris.griffioen@live.nl>
2014-09-15 11:11:13 +0200

    Interaction!

--- www/js/planner.js
+++ www/js/planner.js
@@ -2,3 +2,37 @@
 /*$('#calendar').clndr({
   template: $('#clndr-template').html()
 });*/
+
+function App()
+{
+
+	this.init = function()
+	{
+		$('#avail-days .day').bind('click tap', this.dayClicked);
+		console.log('APP INITIALISED');
+	};
+	
+	this.dayClicked = function()
+	{
+		var el = $(this);
+		if(el.hasClass('available'))
+		{
+			el.removeClass('available');
+		}else{
+			el.addClass('available');
+		}
+		// TODO: Update day to db
+		this.updatePlanner();
+	};
+	
+	this.updatePlanner = function()
+	{
+		// TODO: Update availability bars
+		// TODO: Add Plan buttons on 100% days
+	};
+	
+	
+}
+
+var app = new App();
+app.init();
