a857a374c933961be7fe5b5f704c7a630c7a82c8
GriffinSauce <joris.griffioen@live.nl>
2014-09-15 12:05:04 +0200

    Generate days ui from script

--- www/js/planner.js
+++ www/js/planner.js
@@ -8,10 +8,47 @@ function App()
 
 	this.init = function()
 	{
+		
+		var days = this.getDays();
+		$('#avail-days').append(days);
 		$('#avail-days .day').bind('click tap', this.dayClicked);
+	
 		console.log('APP INITIALISED');
 	};
 	
+	/*	
+	 *	getDays returns days in HTML for the given period
+	 *	no period = this week
+	 */
+	this.getDays = function(period)
+	{	
+		// No period given, write this week
+		if(!period)
+		{
+			var period = new Array(7);
+			for(var i=0; i<7; i++)
+			{
+				period[i] = moment().day(i+1);
+			}
+		}
+		
+		// Build period HTML
+		var periodHMTL = [];
+		for(var i=0; i<period.length; i++)
+		{
+			var day = $('<div class="day"></div>');
+			day.append('<div class="day-name">'+period[i].format('dd')+'</div>')
+			day.append('<div class="day-date">'+period[i].format('DD')+'</div>')
+			periodHMTL.push(day)
+		}
+		
+		return periodHMTL;
+	}
+	
+	/*	
+	 *	Clickhandler for days
+	 *
+	 */
 	this.dayClicked = function()
 	{
 		var el = $(this);
