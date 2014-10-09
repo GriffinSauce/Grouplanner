0faffd569dc49f37f931c9926964248edd283bea
GriffinSauce <joris.griffioen@live.nl>
2014-10-10 00:35:45 +0200

    Implemented SCSS and added Gulp watch task

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass')
+
+gulp.task('sass', function () {
+    gulp.src('./sass/grouplanner.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./www/css'));
+});
+
+gulp.task('watch', function () {
+	gulp.watch('sass/**/*.scss', ['sass']);
+});
