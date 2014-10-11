2adb1499e10505cb8a5e4d7757999cd3c41756a6
GriffinSauce <joris.griffioen@live.nl>
2014-10-11 13:54:25 +0200

    Group creation

--- www/js/create.js
+++ www/js/create.js
@@ -1,7 +1,32 @@
+/*
+ *	Group creation
+ *
+ *
+ */
 $(document).ready(function(){
 	$('#createGroupBtn').click(function(){
-		// TODO: Submit data
-		// TODO: Nav to planner
+		
+		var name = $('#form #name').val();
+		var type = $('#form #type').val();
+		var length = $('#form #length').val();
+		var description = "We're called "+name+" and we plan "+type+" once every "+length+" days.";
+		
+		var data = {
+			name:name,
+			eventtype:type,
+			lenght:length,
+			description:description
+		}
+		$.ajax({
+			url: '/group',
+			type: 'PUT',
+			data: data
+		})
+		.done(function( data ) {
+			console.log( "Returned data:"+ JSON.parse(data) );
+			alert('Success!');
+			window.location.href = '/planner';
+		});
 	})
 	$('.radio .option').click(function(){
 		if(!$(this).hasClass('active'))
