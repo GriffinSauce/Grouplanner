095f7d9e95deccb21cba8d25486f210be0466cca
Niels Overwijn <niels.overwijn@gmail.com>
2014-10-14 07:02:12 +0200

    define global.grouplanner

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ var routes =
 function setUpVariables()
 {
 	//  Set the environment variables we need.
+	global.grouplanner = {};
 	global.grouplanner.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 	global.grouplanner.port      = process.env.OPENSHIFT_NODEJS_PORT || 8085;
 	global.grouplanner.environment = 'remote';
