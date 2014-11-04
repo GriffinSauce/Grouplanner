5dd020eaf6eca58c4b88944cf79c0c6b81e8bd46
Niels Overwijn <niels.overwijn@gmail.com>
2014-11-04 06:50:11 +0100

    Make complete group and user available in template

--- email.js
+++ email.js
@@ -31,9 +31,8 @@ function sendInvite(user, group, invitedUser)
 
 	var data = {
 		subject: 'Invitation from grouplanner',
-		invitee: user.name.first,
-		grouptype: group.eventtype,
-		groupname: group.name,
+		invitee: user,
+		group: group,
 		acceptinvitelink: ''
 	};
 	var body_text = template_text(data);
