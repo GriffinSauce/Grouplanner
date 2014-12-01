d2d1ede8e38af8e46a2a5b87e22f180aedc3bf5a
Niels Overwijn <niels.overwijn@gmail.com>
2014-12-01 07:00:30 +0100

    Better setup for url implemented for Facebook

--- routes/passport.js
+++ routes/passport.js
@@ -15,7 +15,7 @@ var User = require(__dirname + '/../db/user.js');
 router.get('/auth/google', passport.authenticate('google', {scope: 'https://www.googleapis.com/auth/userinfo.email'}));
 router.get('/oauth2callback', passport.authenticate('google', { successRedirect:'loginSuccess', failureRedirect: '/login' }));
 router.get('/auth/facebook', passport.authenticate('facebook', {scope: 'email'}));
-router.get('/oauth2callback-facebook', passport.authenticate('facebook', { successRedirect:'loginSuccess', failureRedirect: '/login' }));
+router.get('/auth/callback/facebook', passport.authenticate('facebook', { successRedirect:'loginSuccess', failureRedirect: '/login' }));
 router.get('/auth/twitter', passport.authenticate('twitter', {scope: 'email'}));
 router.get('/oauth2callback-twitter', passport.authenticate('twitter', { successRedirect:'loginSuccess', failureRedirect: '/login' }));
 
