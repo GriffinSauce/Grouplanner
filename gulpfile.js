805be9ce83581bd038cd1b1b13faa1ad37c6fc05
Niels Overwijn <niels.overwijn@gmail.com>
2015-01-24 20:50:30 +0100

    Added autoprefixer to the gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass')
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
     gulp.src('./sass/grouplanner.scss')
         .pipe(sass())
+		.pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(gulp.dest('./www/css'));
 });
 
