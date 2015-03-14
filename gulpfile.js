939d1fbac0c47961099fadf67beb098b2a410601
GriffinSauce <joris.griffioen@live.nl>
2015-03-14 23:03:00 +0100

    Add live-reload to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass')
 var autoprefixer = require('gulp-autoprefixer');
+var livereload = require('gulp-livereload');
 
 gulp.task('sass', function () {
-    gulp.src('./sass/grouplanner.scss')
-        .pipe(sass())
+	gulp.src('./sass/grouplanner.scss')
+		.pipe(sass())
 		.pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('./www/css'));
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('./www/css'))
+		.pipe(livereload());
 });
 
 gulp.task('watch', function () {
+	livereload.listen();
 	gulp.watch('sass/**/*.scss', ['sass']);
 });
 
