7371e8d55d6c97869d01cc6ba48fc994ae7a5159
Niels Overwijn <niels.overwijn@gmail.com>
2015-03-18 22:19:35 +0100

    Revert "Added custom connection URL - #18"
    This reverts commit ef62a94a976bf20d3e305cc231bea8036d979d6b.

--- server.js
+++ server.js
@@ -48,18 +48,13 @@ if(global.grouplanner.environment == 'local')
 	mongoose.connect('mongodb://' + (process.env.OPENSHIFT_MONGODB_DB_HOST || global.grouplanner.ipaddress) + '/grouplanner');
 } else
 {
-	var dbconnectionURL = 'mongodb://';
-		dbconnectionURL += process.env.MONGODB_USER + ':';
-		dbconnectionURL += env.MONGODB_PASS + '@';
-		dbconnectionURL += global.grouplanner.ipaddress + ':';
-		dbconnectionURL += process.env.OPENSHIFT_MONGODB_DB_PORT + '/';
-		dbconnectionURL += process.env.MONGODB_DB_DB;
-	mongoose.connect(dbconnectionURL);
+	var dbname = process.env.MONGODB_DB || 'grouplanner';
+	mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL + dbname);
 }
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback() { console.log('Connected to the database'); });
+
 
 var sessionMiddleware = session(
 {
@@ -69,7 +64,7 @@ var sessionMiddleware = session(
 		db:mongoose.connection.db
 	}),
 	resave: true,
-	saveUninitialized: true
+    saveUninitialized: true
 });
 
 // EXPRESS SETUP
