8328978071848f6b3744ee8f0ee202ee66c42ec1
HalleyInteractive <niels.overwijn@gmail.com>
2015-03-21 21:17:55 +0100

    Reverse each and self remove detect

--- www/js/handlebars-ext.js
+++ www/js/handlebars-ext.js
@@ -45,3 +45,19 @@ Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
 Handlebars.registerHelper('formatDate', function (date, format) {
 	return moment(date).format(format);
 });
+
+Handlebars.registerHelper('eachReverse', function(context)
+{
+    var options = arguments[arguments.length - 1];
+    var ret = '';
+
+    if (context && context.length > 0) {
+        for (var i = context.length - 1; i >= 0; i--) {
+            ret += options.fn(context[i]);
+        }
+    } else {
+        ret = options.inverse(this);
+    }
+
+    return ret;
+});
