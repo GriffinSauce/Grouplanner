69cac122fdc8235dc2cf00035855b91c8f385c61
GriffinSauce <joris.griffioen@live.nl>
2015-05-10 17:26:24 +0200

    Start refactor on Group and App classes

--- app/js/classes/Group.js
+++ app/js/classes/Group.js
@@ -1,18 +1,242 @@
-/* global $,moment,App,Group,Period */
+/* global $,moment,Period,jshare,localStorage */
 
 /*
- *	input an id for creation
- *	an a ready function to call when loaded
+ *	Group class
  *
  */
-function Group(data)
+function Group()
 {
 	var scope = this;
 
-	// TODO: Build Add member function
-	this.addMember = function()
-	{
+	// Currently active period
+	// Default to this week
+	this.activePeriod = moment().format('DDMMYYYY');
 
+	// Period length
+	// Default 7
+	this.periodLength = this.length;
+
+	// Data contains locally loaded periods, by startDate in DDMMYYYY format
+	// Contains only this week by default
+	this.data = {};
+
+	// User data
+	this.user = {};	// TODO: Move this to App class
+
+	/*
+	 *	Initialise the app
+	 *
+	 */
+	this.init = function()
+	{
+		$('.period-control-button.next').bind('click tap', this.nextPeriod);
+		$('.period-control-button.prev').bind('click tap', this.prevPeriod);
+		$('#invite #send').bind('click tap', this.sendInvite);
+		$('#info #display').bind('click tap', this.editInfo);
+		$('#info #edit .btn',edit).bind('click tap', this.saveInfo);
+		$('.radio .option').bind('click tap',function(){
+			if(!$(this).hasClass('active'))
+			{
+				$(this).siblings('.active').toggleClass('active');
+				$(this).toggleClass('active');
+			}
+		});
+		$('nav .tab').bind('click tap', function(e){
+			var t = $(this)
+
+			// Tabs
+			$('nav .tab').removeClass('active');
+			t.addClass('active');
+
+			// Sections
+			$('section').removeClass('active');
+			$('section#'+t.data('target')).addClass('active');
+
+			// Save
+			localStorage.tab = t.data('target');
+		});
+
+		if(localStorage.tab !== undefined && localStorage.tab !== null)
+		{
+			var tab = localStorage.tab;
+
+			// Tabs
+			$('nav .tab').removeClass('active');
+			$('nav .tab[data-target="'+tab+'"]').addClass('active');
+
+			// Sections
+			$('section').removeClass('active');
+			$('section#'+tab).addClass('active');
+		}
+
+		$('.remove-user').bind('click tap', scope.removeMember);
+
+		// Get data that is supplied with jshare
+		scope.user = jshare.user;
+
+		// Save supplied group data into this object
+		for(var key in jshare.group)
+		{
+			scope[key] = jshare.group[key];
+		}
+
+		scope.renderEvents();
+		console.log('APP INITIALISED');
+	};
+
+	/*
+	 *	Load a particular period
+	 *	Input a startDate as a DDMMYYYY string
+	 */
+	this.loadPeriod = function(p)
+	{
+		this.activePeriod = p;
+		// TODO: get data from DB
+		// If Period doesn't exist, create new
+		if(typeof scope.data[scope.activePeriod] === 'undefined')
+		{
+			scope.data[scope.activePeriod] = new Period({startDate:scope.activePeriod, length:scope.periodLength}, scope);
+		}
+		scope.data[scope.activePeriod].activate();
+		scope.updateName();
+	};
+
+	/*
+	 *	Switch to next period and load
+	 *
+	 */
+	this.nextPeriod = function()
+	{
+		console.log('Loading next period');
+		// Get next period in DDMMYYYY string
+		var next = moment(scope.activePeriod, 'DDMMYYYY').add(scope.periodLength, 'd').format('DDMMYYYY');
+		scope.loadPeriod(next);
+	};
+
+	/*
+	 *	Switch to previous period and load
+	 *
+	 */
+	this.prevPeriod = function()
+	{
+		console.log('Loading previous period');
+		// Get next period in DDMMYYYY string
+		var prev = moment(scope.activePeriod, 'DDMMYYYY').subtract(scope.periodLength, 'd').format('DDMMYYYY');
+		scope.loadPeriod(prev);
+	};
+
+	/*
+	 *	Update period display
+	 *
+	 */
+	this.updateName = function()
+	{
+		// Update period name display
+		var startDay = scope.data[scope.activePeriod].startDate.format('D MMM');
+		var endDay = scope.data[scope.activePeriod].endDate.format('D MMM');
+		var periodName = startDay+' - '+endDay;
+		$('#period-controls .period span').text(periodName);
+	};
+
+	/*
+	 *	Show invite page
+	 *
+	 */
+	this.sendInvite = function()
+	{
+		var email = $('#invite input').val();
+		var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+		if(re.test(email))
+		{
+			var data = {
+				invitedUser:{email:email},
+				group:scope._id
+			};
+			socket.emit('put/invite', data, function(rtnData) {
+				if(rtnData.success)
+				{
+					// TODO: notify user of success more smoothly
+					$('#invite input').val('');
+					$('#invite #send i').removeClass('icon-mail').addClass('icon-check');
+					setTimeout(function(){
+						$('#invite #send i').removeClass('icon-check').addClass('icon-mail');
+					},1500);
+				}else{
+					alert('An error occured, please try again later.');
+				}
+			});
+		}else{
+			alert("Please provide a valid e-mail address");
+		}
+	};
+
+	/*
+	 *	Edit group info
+	 *
+	 */
+	this.editInfo = function()
+	{
+		$('#info #display').removeClass('visible');
+		$('#info #edit').addClass('visible');
+	};
+
+	/*
+	 *	Save group info
+	 *
+	 */
+	this.saveInfo = function()
+	{
+		$('#info #edit').removeClass('visible');
+		$('#info #display').addClass('visible');
+
+		if(validate())
+		{
+			var name = $('#info #edit #name').val();
+			var type = $('#info #edit #type').val();
+			//var length = $('#info #edit #length').val();
+			var data = {
+				id:scope._id,
+				name:name,
+				eventtype:type,
+				permissions:
+				{
+					plan:
+					{
+						addNewMembers:JSON.parse($('.radio#planning .active').attr('id'))
+					},
+					settings:
+					{
+						addNewMembers:JSON.parse($('.radio#settings .active').attr('id'))
+					}
+				}
+			}
+			socket.emit('update/group', {group:data,user:jshare.user}, function(rtnData) {
+				if(rtnData.success)
+				{
+					// Update client side
+					$('#header #back span').text(rtnData.group.name);
+					$('#info #display h2').text(rtnData.group.name);
+					$('#info #display p').text(rtnData.group.description);
+					scope.name = rtnData.group.name;
+					scope.eventtype = rtnData.group.eventtype;
+					scope.permissions.plan.addNewMembers = rtnData.group.permissions.plan.addNewMembers;
+					scope.permissions.settings.addNewMembers = rtnData.group.permissions.settings.addNewMembers;
+				}else{
+					alert('There was an error, admins have not been notified. Sucks to be you.'); // lol
+				}
+			});
+		}
+
+		function validate()
+		{
+			if($('#info #edit #name').val().length === 0)
+			{
+				alert('A name is required.');
+				$('#info #edit').focus();
+				return false;
+			}
+			return true;
+		}
 	};
 
 	/**
@@ -28,7 +252,7 @@ function Group(data)
 		} else
 		{
 			var memberToRemove = {};
-			$.each(app.group.members, function(index, member)
+			$.each(scope.members, function(index, member)
 			{
 				if(member._id == userId)
 				{
@@ -53,15 +277,13 @@ function Group(data)
 		}
 	};
 
-	// TODO: Build Delete group function
-	this.delete = function()
+	this.renderEvents = function()
 	{
-
+		var compiledTemplate = Handlebars.getTemplate('events');
+		var html = $(compiledTemplate(scope.events));
+		$('section#updates').html(html);
 	};
 
-	// Save supplied data into this object
-	for(var key in data)
-	{
-		scope[key] = data[key];
-	}
+
+	this.init();
 }
--- app/js/group.js
+++ app/js/group.js
@@ -9,40 +9,12 @@ Handlebars.registerHelper("everyOther", function (index, amount, scope) {
 
 var socket = io();
 
-var app = new App();
+var group = new Group();
 // Load this week
 var thisWeek = moment().weekday(1).format('DDMMYYYY');
-app.loadPeriod(thisWeek);
+group.loadPeriod(thisWeek);
 
 // header
 $(document).ready(function(){
-	$('nav .tab').bind('click tap', function(e){
-		var t = $(this)
 
-		// Tabs
-		$('nav .tab').removeClass('active');
-		t.addClass('active');
-
-		// Sections
-		$('section').removeClass('active');
-		$('section#'+t.data('target')).addClass('active');
-
-		// Save
-		localStorage.tab = t.data('target');
-	});
-
-	if(localStorage.tab !== undefined && localStorage.tab !== null)
-	{
-		var tab = localStorage.tab;
-
-		// Tabs
-		$('nav .tab').removeClass('active');
-		$('nav .tab[data-target="'+tab+'"]').addClass('active');
-
-		// Sections
-		$('section').removeClass('active');
-		$('section#'+tab).addClass('active');
-	}
-
-	$('.remove-user').bind('click tap', app.group.removeMember);
 });
