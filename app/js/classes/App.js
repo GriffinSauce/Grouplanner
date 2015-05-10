abe7cd1ff30a5f14200af616b0700377fff443e3
GriffinSauce <joris.griffioen@live.nl>
2015-05-10 17:35:50 +0200

    Start new App class

--- app/js/classes/App.js
+++ app/js/classes/App.js
@@ -1,9 +1,17 @@
+/* global $,moment,Period,jshare,localStorage */
+
 /*
+ *	App.js should contain:
+ *	- user info
+ *	- references to groups of this user
+ *	- global user/app related functions
+ *
+ */
+function App()
+{
+	// User data
+	this.user = {};
 
-App.js should contain:
-
-- user info
-- references to groups of this user
-- global user/app related functions
-
-*/
+	// Groups
+	this.groups = [];
+}
