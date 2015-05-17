71e07d9974f9c2a31bbb5100c5d62696f2da3ad9
Joris Griffioen <joris@peerby.com>
2015-05-17 15:44:22 +0200

    Routes and groups

--- app/assets/js/app.module.js
+++ app/assets/js/app.module.js
@@ -1,17 +1,22 @@
 var app = angular.module('grouplanner', ['ngRoute']);
 
 app.config(
-    function($routeProvider) {
-    $routeProvider.
-      when('/', {
-        templateUrl: 'components/home/home-view.html',
-        controller: 'HomeCtrl'
-      }).
-      when('/app/groups', {
-        templateUrl: 'components/groups/groups-view.html',
-        controller: 'GroupsCtrl'
-      }).
-      otherwise({
-        redirectTo: '/'
-      });
-});
+	function($routeProvider, $locationProvider) {
+		$routeProvider.
+		when('/', {
+			templateUrl: 'components/home/home-view.html',
+			controller: 'HomeCtrl'
+		}).
+		when('/groups', {
+			templateUrl: 'components/groups/groups-view.html',
+			controller: 'GroupsCtrl'
+		}).
+		otherwise({
+			redirectTo: '/'
+		});
+		
+		// enable html5Mode for pushstate ('#'-less URLs)
+		$locationProvider.html5Mode(true);
+		$locationProvider.hashPrefix('!');
+	}
+);
