154c8cade6477465505bd1920cab114881b6c91a
Joris Griffioen <joris@peerby.com>
2015-05-17 14:26:20 +0200

    Hello world! :D

--- app/assets/js/app.module.js
+++ app/assets/js/app.module.js
@@ -1,1 +1,13 @@
-var app = angular.module('grouplanner', []);
+var app = angular.module('grouplanner', ['ngRoute']);
+
+app.config(
+    function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'components/home/home-view.html',
+        controller: 'HomeCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+});
