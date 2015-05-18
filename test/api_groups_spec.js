6540f91d17a817360973ed4750f9700825143a86
Niels Overwijn <niels.overwijn@gmail.com>
2015-05-18 07:13:14 +0200

    Added frisby tests

---
+++ test/api_groups_spec.js
@@ -0,0 +1,9 @@
+var frisby = require('frisby');
+var Group = require(__dirname + '/../server/mongoose/group.js');
+
+frisby.create('Groups test')
+  .get('http://localhost:8085/api/groups')
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSONTypes(Group.schema.tree)
+.toss();
---
+++ test/api_spec.js
@@ -0,0 +1,16 @@
+var frisby = require('frisby');
+
+/**
+* This test is to check if the api respons with a
+* 401 (Unauthorized) to all routes before any login has occurred.
+**/
+
+frisby.create('Query groups')
+  .get('http://localhost:8085/api/groups')
+  .expectStatus(401)
+.toss();
+
+frisby.create('Specific group')
+  .get('http://localhost:8085/api/groups/jhgjghjhg')
+  .expectStatus(401)
+.toss();
