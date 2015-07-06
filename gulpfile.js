f8d3ea8db54fdf34b358cf7f64a4a93c3f4b3295
Joris Griffioen <joris@peerby.com>
2015-07-06 18:15:20 +0200

    Do gulpy things

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,43 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass')
 var autoprefixer = require('gulp-autoprefixer');
-var livereload = require('gulp-livereload');
+var browserSync = require('browser-sync').create();
+var server = require( 'gulp-develop-server');
+var plumber = require('gulp-plumber');
 
 gulp.task('sass', function () {
 	gulp.src('./app/assets/sass/app.scss')
+        .pipe(plumber())
 		.pipe(sass())
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions'],
 			cascade: false
 		}))
 		.pipe(gulp.dest('./app/assets/css'))
-		.pipe(livereload());
+        .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function () {
-	livereload.listen();
+gulp.task('watch', ['browser-sync'], function () {
 	gulp.watch('./app/assets/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('browser-sync', ['server:start'], function() {
+    browserSync.init(null, {
+        proxy: 'localhost:8085',
+        files: ['client/css'],
+        port: 3000,
+        open: false,
+        notify: true
+    });
+});
+gulp.task( 'server:start', function(cb) {
+    server.listen({
+        path: 'server.js'
+    }, function( error ) {
+        if( ! error ){
+            cb();
+        }
+    });
 });
 
 gulp.task('default', ['watch']);
